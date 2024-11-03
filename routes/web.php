f5e9d6a62cf574e48dd23c33e41cb465c4ea236f
haikaljs <haikaljavascript@gmail.com>
2024-11-03 15:06:18 +0800

    Forget, reset password

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,21 @@ Route::get('/', function () {
 Route::middleware('admin')->group(function () {
     
     Route::get('/profile',[AdminAuthController::class,'profile'])->name('admin_profile');
+
     Route::get('/logout',[AdminAuthController::class,'logout'])->name('admin_logout');
+
     Route::get('/dashboard',[AdminDashboardController::class,'dashboard'])->name('admin_dashboard');
 
 });
 Route::prefix('admin')->group(function () {
+    Route::get('/', function () {return redirect('/admin/login');});
     Route::get('/login',[AdminAuthController::class,'login'])->name('admin_login');
     Route::post('/login',[AdminAuthController::class,'login_submit'])->name('admin_login_submit');
+    Route::get('/logout',[AdminAuthController::class,'logout'])->name('admin_logout');
     Route::get('/forget-password',[AdminAuthController::class,'forget_password'])->name('admin_forget_password');
-    Route::get('/reset-password',[AdminAuthController::class,'reset_password'])->name('admin_reset_password');
-    Route::get('/profile',[AdminAuthController::class,'profile'])->name('admin_profile');
-    Route::get('/dashboard',[AdminDashboardController::class,'dashboard'])->name('admin_dashboard');
-
+    Route::post('/forget_password_submit',[AdminAuthController::class,'forget_password_submit'])->name('admin_forget_password_submit');
+    Route::get('/reset-password/{token}/{email}',[AdminAuthController::class,'reset_password'])->name('admin_reset_password');
+    Route::post('/reset-password/{token}/{email}',[AdminAuthController::class,'reset_password_submit'])->name('admin_reset_password_submit');
 });
+
 
